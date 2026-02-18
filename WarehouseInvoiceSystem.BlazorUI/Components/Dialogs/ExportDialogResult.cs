bba7b6aee526ada053cf91420747b934b322a65b
Jovan Tashkov <jovcotaskov@gmail.com>
2026-02-18 20:27:29 +0100

    updates

--- WarehouseInvoiceSystem.BlazorUI/Components/Dialogs/ExportDialogResult.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace WarehouseInvoiceSystem.BlazorUI.Components.Dialogs
-{
-    public class ExportDialogResult
-    {
-        public bool ExportAll { get; set; }
-        public DateTime StartDate { get; set; }
-        public DateTime EndDate { get; set; }
-    }
-}
